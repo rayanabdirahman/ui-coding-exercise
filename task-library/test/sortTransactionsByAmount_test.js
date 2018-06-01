8052dd16fab97b389bd50f6bab7c097cc2c166f2
Rayan Abdirahman <rayanmabdirahman@gmail.com>
2018-06-01 22:25:28 +0100

    initial commit for task-library

---
+++ task-library/test/sortTransactionsByAmount_test.js
@@ -0,0 +1,14 @@
+/*
+ * Test suits
+ */
+
+// require expect function from chai
+const expect = require('chai').expect;
+
+// sanity check
+describe('Sanity check for mocha', () => {
+  it('should run test using npm test', () => {
+    expect(true).to.be.ok;
+  })
+});
+
