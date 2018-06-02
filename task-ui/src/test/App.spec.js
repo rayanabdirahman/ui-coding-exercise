5eb197f618b33e7254c89f31a75a24861e73b58a
Rayan Abdirahman <rayanmabdirahman@gmail.com>
2018-06-02 17:55:09 +0100

    Add enzyme and babel-register for unit tests

--- task-ui/src/test/App.spec.js
+++ task-ui/src/test/App.spec.js
@@ -1,12 +1,3 @@
-// sanity check
-const expect = require('chai').expect;
-
-function add (a, b) {
-    return a + b;
-};
-
-describe('add', () => {
-    it('should add positive numbers', () => {
-        expect(add(1,2)).to.equal(3);
-    });
-});
+// import test dependencies
+import React from 'react';
+import { expect } from 'chai';
