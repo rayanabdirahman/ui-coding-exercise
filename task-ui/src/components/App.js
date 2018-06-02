1c2c4bcc1d414a3410838a8505678d16debc97f3
Rayan Abdirahman <rayanmabdirahman@gmail.com>
2018-06-02 17:42:26 +0100

    import task library functions

--- task-ui/src/components/App.js
+++ task-ui/src/components/App.js
@@ -1,4 +1,15 @@
+// import react dependencies
 import React, { Component } from 'react';
+
+// import sortTransactionsByAmount from task-library
+import { sortTransactionsByAmount } from 'task-library/sortTransactionsByAmount';
+
+// import searchTransactionsByName from task-library
+import { searchTransactionsByName } from 'task-library/searchTransactionsByName';
+
+// URL to transactions
+const ENDPOINT = 'http://localhost:3000/transactions';
+
 
 class App extends Component {
   render() {
